ccb6c1b73295ad6533a79b9467c3eea2e1823c32
AJ Raftis <araftis@calpoly.edu>
2021-01-25 20:18:01 -0800

    Initial import.

---
+++ AI Explorer/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  AI Explorer
+//
+//  Created by AJ Raftis on 1/12/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    }
+
+}
+
